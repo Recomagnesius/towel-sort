a7d6f66ca14079c148c18cd1917e76d12c984c8d
Recomagnesius <buzhor.danyl@gmail.com>
2021-03-10 09:31:06 +0200

    fix: gitignore

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort(matrix) {
+// module.exports =
+  function towelSort(matrix) {
   let m2 = [];
-  for (let i = 0; i != matrix.length; i++){
-    if (i % 2 == 0) {
-      m2 = m2.concat(matrix[i].reverse);
-    }
-    else if (let i = 0; i != matrix.length; i++) {
-      m2 = m2.concat(matrix[i]);
+  if (matrix.length == 0 && matrix == undefined) {
+    return [];
+  }
+  else {
+    for (let i = 0; i < matrix.length; i++){
+      if (i % 2 == 1 || i == 1) {
+        m2 = m2.concat(matrix[i].reverse);
+        console.log(m2);
+      }
+      else{
+        m2 = m2.concat(matrix[i]);
+        console.log(m2);
+      }
     }
   }
-
   return m2;
 }
-console.log(towelSort([
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]));
+// console.log(towelSort([
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ]));
